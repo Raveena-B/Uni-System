29ebc081d3073c7f4ff3100359f73c46368666dd
Raveena Bandara <raveenabandara801@gmail.com>
2022-07-08 12:33:23 +0530

    update index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+const Koa = require("koa");
+const KoaRouter = require("koa-router");
+const cors = require("@koa/cors");
+const bodyparser = require("koa-bodyparser");
+const json = require("koa-bodyparser");
+
+const app = new Koa(); // create the new koa app
+const router = new KoaRouter(); //initialize the koa router
+
+//use the middlewares
+app.use(cors());
+app.use(bodyparser());
+app.use(json());
+app.use(router.routes()).use(router.allowedMethods());
+
+// create the Root router ( use the bodyparser - massage of the body)
+router.get("/", (ctx) => {
+  //(node -> (req,res,next) but koa -> (ctx))  Encapsiulation
+  ctx.body = { message: "Server Mangement API" };
+});
+
+//app listen function
+app.listen(9000, () => {
+  console.log(`Server is up and running http://localhost:9000`);
+});
